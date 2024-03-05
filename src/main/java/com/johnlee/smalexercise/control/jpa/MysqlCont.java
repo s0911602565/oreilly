a72a9eb2d5262437560ce8d055d43fa4b307e2e1
s0911602565 <s0911602565@gmail.com>
2024-03-05 19:04:24 +0800

    20240305_1705

--- src/main/java/com/johnlee/smalexercise/control/jpa/MysqlCont.java
+++ src/main/java/com/johnlee/smalexercise/control/jpa/MysqlCont.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.johnlee.smalexercise.bean.x2;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.sql.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("MysqlCont")
@@ -51,5 +54,43 @@ public class MysqlCont {
         x.setName("kptv");
         System.out.println("ex save");
         return c.save(x);
+    }
+
+    /*
+    http://127.0.0.1:8080/MysqlCont/setInfo
+    */
+    @RequestMapping("test")
+    public List<x2> setInfo(){
+        x2 a1 = new x2();
+        a1 = new x2();
+
+        a1.setName("john3");
+
+        x2 a2 = new x2();
+        a2 = new x2();
+
+        a2.setName("john4");
+        List<x2> list = List.of(a1 , a2);
+        return list;
+    }
+
+    /*
+    http://127.0.0.1:8080/MysqlCont/setInfo
+    */
+    @RequestMapping("setInfo")
+    public String TestClint(){
+        try{
+            WebClient web = WebClient.create("http://127.0.0.1:8080/MysqlCont/test");
+            web.get()
+            .retrieve()
+            .bodyToFlux(x2.class)
+            .toStream()
+            .forEach(c ::save);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        return "success";
     }
 }
